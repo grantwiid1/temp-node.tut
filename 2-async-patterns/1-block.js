5d4084d1e3520c126f82bd854334838e6d04fb60
Grant Wiid <grantwiid@gmail.com>
2022-11-22 19:48:36 +0200

    update 14

---
+++ 2-async-patterns/1-block.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.end('Home page')
+        return
+    }
+    if(req.url === '/about'){
+        for(let i =0; i< 100; i++){
+            for(let j = 0; j< 100; j++){
+                console.log(`${i} ${j}`)
+            }
+        }
+        res.end('About Page')
+        return
+    }
+    res.end('Error')
+})
+
+server.listen(5000, () => {
+    console.log(`Listening on port: 5000....`)
+})
